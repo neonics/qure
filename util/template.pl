3f6609bdfcef703c4607263df927a418b31be579
kenney <kenney@neonics.com>
2013-11-18 08:14:32 +0100

    Web DOC Updates.
    DOC/*.txt:
    	fix layout for util/txt2html.pl, add minor notes here and there,
    	add some links to local html docs where appropriate.
    
    	Debugging.txt:
    		add a few notes on current capabilities;
    
    	Filesystem.txt:
    		Add notes on scheduling optimization, handle ordering
    		in both volatile and persistent memory, and a generic
    		solution optimizing disk layout by OS usage;
    
    	PersistentCluster.html:
    		remove including style.css;
    
    Makefile:
    	Web-Document generation updates:
    
    	Add TXTDOC: OOFS Security Fork Storage Paging Scheduling ITC
    	GAS GitFS Debugging Architecture
    
    	HTML generation: use new features of txt2html.pl to generate
    	the html in one step (no intermediate file).
    
    	add target root/www/doc/menu.xml (and dep in HTMLDOC),
    	copying root/www/doc-menu.xml (as the doc/ directory is deleted
    	on clean).
    
    	doc.inc: don't include xml files;
    
    	generate root/www/doc/index.html (including a modified version of
    	doc.inc).
    
    root/www/index.html:
    	change tagline;
    	update link to OOFS doc.
    
    util/template.html:
    	add <title> tag and <h1> for new ${TITLE}.
    	add <h2> for TOC and new ${TOC}.
    
    util/template.pl:
    	add option --menuxml specifying a menu.xml override to pass to
    	js/template.js template() function;
    
    	add option --title to specify a document title;
    
    	add option --tagline to specify a ${TAGLINE} value
    	(not in any template as yet).
    
    	Replace ${TOC} (with undefined $toc as yet), ${TITLE} and ${TAGLINE}.
    
    util/txt2html.pl:
    	recode option parsing;
    	add options:
    		--toc to prepend the TOC contents on raw output (-t none);
    
    		--title to se the value for ${TITLE};
    
    		--rawtitle which takes a DOC/FooBar.txt transforming
    		it into "Foo Bar" (only the top level dir is stripped)
    		and treats the result as --title;
    
    		-p to set ${RP} (relative path for css/js) like template.pl;
    
    		--onload to specify the entire onload javascript string to
    		inject into ${ONLOAD};
    
    		--tagline to replace ${TAGLINE}.
    
    	generate a hierarchical TOC scanning all <h.> tags and replace ${TOC}
    	in the template (--toc not needed).
    
    	builtin template: add <title>, <h1>, and toc.

--- util/template.pl
+++ util/template.pl
@@ -1,11 +1,13 @@
 #!/usr/bin/perl
-
 $xml;
 $relpath="";
 $template = "template.html";
+$menuxml = undef;
+$title="";
+$tagline="Conscious Computing";
 @styles;
 
-while ( $ARGV[0] =~ /^-/ )
+while ( $ARGV[0] =~ /^-./ )
 {
 	my $a = shift @ARGV;
 	$a eq '-t' and $template = shift @ARGV or
@@ -13,19 +15,28 @@ while ( $ARGV[0] =~ /^-/ )
 	$a eq '-s' and push @styles, shift @ARGV or
 	$a eq '-p' and $relpath = shift @ARGV or
 	$a eq '--onload' and $onload .= shift @ARGV or
+	$a eq '--menuxml' and $menuxml .= shift @ARGV or
+	$a eq '--title' and $title .= shift @ARGV or
+	$a eq '--tagline' and $tagline .= shift @ARGV or
 	die "unknown option: $a";
 }
 
 $xml = defined($xml) ? "'$xml'" : "null";
 
-$onload="template( $xml, '$relpath', [".join(",", map{ "'$_'"} @styles)."]); $onload";
+$onload="template( $xml, '$relpath', [".join(",", map{ "'$_'"} @styles)."]"
+	.(defined $menuxml ? ", '$menuxml'" : "")
+	."); $onload"
+unless $onload;
 
 $c=readfile( $template );
 $content= defined $ARGV[0] ? readfile( $ARGV[0] ) : "";
 
 $c=~s/\$\{CONTENT\}/$content/ge;
+$c=~s/\$\{TOC\}/$toc/ge;
 $c=~s/\$\{ONLOAD\}/$onload/ge;
 $c=~s/\$\{RP\}/$relpath/ge;
+$c=~s/\$\{TITLE\}/$title/ge;
+$c=~s/\$\{TAGLINE\}/$tagline/ge;
 
 print $c;
 
